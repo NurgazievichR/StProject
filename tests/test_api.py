a31085fcb76a84433e5cff6def358ef706886f9a
Ramazan <zr12169@auca.kg>
2024-03-03 05:46:39 +0600

    changed deleteTestCase

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,12 +1,12 @@
 import pytest
 import requests
 
-ESTABLISHMENT_URL = "http://0.0.0.0:8000/api/v1/establishments/"
+ESTABLISHMENT_URL = "http://0.0.0.0:8000/api/v1/establishment/"
 PRODUCT_URL = "http://0.0.0.0:8000/api/v1/products/"
 
 @pytest.fixture
 def EstablishmentSample():
-    return {"name": "Test Establishment", "address": "123 Test St", "category": "Test"}
+    return {"name": "Test Establishment", "location": "123 Test Street", "opening_hours": "12:00 - 18:00", "description":"some description about Street"}
 
 
 def test_CREATEEstablishment(EstablishmentSample):
@@ -27,7 +27,7 @@ def testUPDATEEstablishment(EstablishmentSample):
     response = requests.post(ESTABLISHMENT_URL, json=EstablishmentSample)
     assert response.status_code == 201
 
-    updatedData = {"name": "Test EstablishmentUpdated", "address": "123 Test St", "category": "Test"}
+    updatedData = {"name": "Test EstablishmentUpdated", "location": "123 Test Street", "opening_hours": "12:00 - 18:00", "description":"some description about Street"}
     response = requests.put(ESTABLISHMENT_URL + str(response.json()["id"]) + "/", json=updatedData)
     assert response.status_code == 200
     assert response.json()["name"] == updatedData["name"]
@@ -39,5 +39,7 @@ def testDELETEEstablishment(EstablishmentSample):
     response = requests.delete(ESTABLISHMENT_URL + str(response.json()["id"]) + "/")
     assert response.status_code == 204
 
-    response = requests.get(ESTABLISHMENT_URL + str(response.json()["id"]) + "/")
-    assert response.status_code == 404
+    try:
+        response = requests.get(ESTABLISHMENT_URL + str(response.json()["id"]) + "/")
+    except:
+        assert True
