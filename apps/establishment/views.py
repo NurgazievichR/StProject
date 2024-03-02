2f567d62e5196d7e2d807e2d33a8989cab73d757
Ramazan <zr12169@auca.kg>
2024-03-02 23:14:00 +0600

    added pagination

--- apps/establishment/views.py
+++ apps/establishment/views.py
@@ -1,4 +1,5 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Establishment
 from .serializers import EstablishmentSerializer
--- apps/product/views.py
+++ apps/product/views.py
@@ -1,11 +1,13 @@
 from rest_framework import viewsets
+from rest_framework.pagination import PageNumberPagination
 
 from .models import Product
 from .serializers import ProductSerializer
 
+
 class ProductViewSet(viewsets.ModelViewSet):
     queryset = Product.objects.all()
     serializer_class = ProductSerializer
-
+    
 
     
