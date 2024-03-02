0e4bfee27aeb6dae8af4878f08b33e7bfce90983
Ramazan <zr12169@auca.kg>
2024-03-02 21:36:33 +0600

    add Product and Establishment Models

--- apps/establishment/models.py
+++ apps/establishment/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Establishment(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    opening_hours = models.CharField(max_length=100)
+
+    def __str__(self) -> str:
+        return self.name
+    
+    
