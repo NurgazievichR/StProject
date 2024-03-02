31d725bbc2c63e56eb254ae6f6b2b832a6f01927
Ramazan <zr12169@auca.kg>
2024-03-03 04:34:07 +0600

    unregister User Group

--- apps/establishment/admin.py
+++ apps/establishment/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
+from django.contrib.auth.models import User, Group
 
 from .models import Establishment
 
+
+admin.site.unregister(User)
+admin.site.unregister(Group)
 admin.site.register(Establishment)
